56fb735094b0ccef274a5edff5dd74427cfe1129
Aldus Daniel Ferreira de Souza Monteiro <aldusdanielfsm@gmail.com>
2023-07-10 13:36:38 -0300

    Update HealthControllerTest.java

--- src/test/java/com/coffeenances/api/controllers/HealthControllerTest.java
+++ src/test/java/com/coffeenances/api/controllers/HealthControllerTest.java
@@ -18,7 +18,7 @@ public class HealthControllerTest {
     @Test
     public void testHealth () throws Exception {
         mockMvc.perform(get("/health").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+                .andExpect(MockMvcResultMatchers.status().isOk());
 
 
     }
