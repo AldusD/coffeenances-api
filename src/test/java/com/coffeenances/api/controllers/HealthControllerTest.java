de529824210aa39ca198a644d56881057da3b837
aldusd <aldusdanielfsm@gmail.com>
2023-07-10 12:58:56 -0300

    fix: returns test code

--- src/test/java/com/coffeenances/api/controllers/HealthControllerTest.java
+++ src/test/java/com/coffeenances/api/controllers/HealthControllerTest.java
@@ -17,8 +17,8 @@ public class HealthControllerTest {
 
     @Test
     public void testHealth () throws Exception {
-//        mockMvc.perform(get("/health").contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform(get("/health").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
 
 
     }
